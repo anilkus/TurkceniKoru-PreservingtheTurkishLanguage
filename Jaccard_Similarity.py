e3dea026a76cc39df80691c14ec9c55e201f278a
Anıl Kuş <anilkus01@gmail.com>
2024-05-20 09:47:06 +0300

    Update Jaccard_Similarity.py

--- Jaccard_Similarity.py
+++ Jaccard_Similarity.py
@@ -2,29 +2,29 @@ import pandas as pd
 from sklearn.metrics import jaccard_score
 from sklearn.feature_extraction.text import CountVectorizer
 
-# Excel dosyasını oku
-df = pd.read_excel("C:/Users/LENOVO/Desktop/Tarsus Bildiri/Yeni Bildiri/11 Mayıs/Sonuçlar.xlsx")  # Excel dosyasının adını ve yolunu belirtin
+# read the excel file
+df = pd.read_excel("ExcelPath")  # Excel dosyasının adını ve yolunu belirtin
 
-# Belirli hücrelerdeki cümleleri al
+# Excel Cells
 cumle1 = df.iloc[7, 2]  # 9. satır, 2. sütun (0'dan başlayan indeksleme)
 cumle2 = df.iloc[7, 3]  # 9. satır, 3. sütun (0'dan başlayan indeksleme)
 
-# Cümlelerin kelime kümesini oluştur
+# Sentences
 vectorizer = CountVectorizer(binary=True)
 vectorizer.fit([cumle1, cumle2])
 cumle1_vector = vectorizer.transform([cumle1])
 cumle2_vector = vectorizer.transform([cumle2])
 
-# Jaccard Similarity hesapla
+# Jaccard Similarity calculation
 jaccard_sim = jaccard_score(cumle1_vector.toarray()[0], cumle2_vector.toarray()[0])
 
-# Sonucu göster
+# Result
 print("Jaccard Similarity:", jaccard_sim)
 
-#excele kaydetme
+#Saving in Excel
 '''
 from openpyxl import load_workbook
-wb = load_workbook('C:/Users/LENOVO/Desktop/Tarsus Bildiri/Yeni Bildiri/11 Mayıs/Sonuçlar.xlsx')
+wb = load_workbook('FilePath')
 ws = wb['Sheet1']
 
 
@@ -32,6 +32,6 @@ ws.cell(row=3,column=7).value = jaccard_sim
 
 
 
-wb.save('C:/Users/LENOVO/Desktop/Tarsus Bildiri/Yeni Bildiri/11 Mayıs/Sonuçlar.xlsx')
+wb.save('FilePath')
 
 '''
