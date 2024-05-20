6d4ade38081f48143e5855911e00d0295c1e4f38
Anıl Kuş <anilkus01@gmail.com>
2024-05-20 09:45:53 +0300

    Update cosine_similarity.py

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -3,29 +3,29 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Excel dosyasını oku
-df = pd.read_excel("C:/Users/LENOVO/Desktop/Tarsus Bildiri/Yeni Bildiri/11 Mayıs/Sonuçlar.xlsx")  # Excel dosyasının adını ve yolunu belirtin
+df = pd.read_excel("filePath")  # Excel dosyasının adını ve yolunu belirtin
 
-# Belirli hücrelerdeki cümleleri al
+# Excel cells
 cumle1 = df.iloc[8, 2]  # 9. satır, 2. sütun (0'dan başlayan indeksleme)
 cumle2 = df.iloc[8, 3]  # 9. satır, 3. sütun (0'dan başlayan indeksleme)
 
-# TfidfVectorizer kullanarak cümle vektörlerini oluştur
+# TfidfVectorizer 
 vectorizer = TfidfVectorizer()
 vectorizer.fit([cumle1, cumle2])
 cumle1_vector = vectorizer.transform([cumle1])
 cumle2_vector = vectorizer.transform([cumle2])
 
-# Cosine Similarity hesapla
+# Cosine Similarity calculation
 cos_sim = cosine_similarity(cumle1_vector, cumle2_vector)
 
-# Sonucu göster
+# Result
 print("Cosine Similarity:", cos_sim[0][0])
 
 
-#excele kaydetme
+#Save in Excel
 '''
 from openpyxl import load_workbook
-wb = load_workbook('C:/Users/LENOVO/Desktop/Tarsus Bildiri/Yeni Bildiri/11 Mayıs/Sonuçlar.xlsx')
+wb = load_workbook('FilePath')
 ws = wb['Sheet1']
 
 
@@ -33,6 +33,6 @@ ws.cell(row=3,column=6).value = cos_sim[0][0]
 
 
 
-wb.save('C:/Users/LENOVO/Desktop/Tarsus Bildiri/Yeni Bildiri/11 Mayıs/Sonuçlar.xlsx')
+wb.save('FilePath')
 
 '''
